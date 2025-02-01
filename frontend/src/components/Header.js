d5dd3b24f51ae99160f8114bdc93cba90f6b8186
Yogi-Gowda <yogeshkumar20369@gmail.com>
2025-02-01 14:32:15 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,30 +7,30 @@ const Header = () => {
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" }); // Smooth scrolling
+      section.scrollIntoView({ behavior: "smooth" });
     }
-    setIsMenuOpen(false); // Close the menu after clicking a link
+    setIsMenuOpen(false);
   };
 
   return (
     <header className="header">
-      {/* Hamburger menu for mobile and minimized desktop */}
+      {/* Hamburger menu (Visible in Mobile) */}
       <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
         <div className="line"></div>
         <div className="line"></div>
         <div className="line"></div>
       </div>
 
-      {/* Desktop Navigation menu */}
+      {/* Desktop Navigation (Visible only in Large Screens) */}
       <ul className="navbar">
-        <li onClick={() => scrollToSection("hero")}>Home</li>
-        <li onClick={() => scrollToSection("projects")}>Projects</li>
-        <li onClick={() => scrollToSection("blogs")}>Blogs</li>
-        <li onClick={() => scrollToSection("certifications")}>Certifications</li>
-        <li onClick={() => scrollToSection("contact")}>Contact Us</li>
+        <li onClick={() => scrollToSection("hero")}><b>Home</b></li>
+        <li onClick={() => scrollToSection("projects")}><b>Projects</b></li>
+        <li onClick={() => scrollToSection("blogs")}><b>Blogs</b></li>
+        <li onClick={() => scrollToSection("certifications")}><b>Certifications</b></li>
+        <li onClick={() => scrollToSection("contact")}><b>Contact Me</b></li>
       </ul>
 
-      {/* Mobile Navigation menu */}
+      {/* Mobile Navigation */}
       <ul className={`mobile-menu ${isMenuOpen ? 'open' : ''}`}>
         <li onClick={() => scrollToSection("hero")}>Home</li>
         <li onClick={() => scrollToSection("projects")}>Projects</li>
