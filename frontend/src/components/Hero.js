5a6c356448a8d69526d1e423970c974e2a3bf7f3
Yogi-Gowda <yogeshkumar20369@gmail.com>
2025-02-01 15:05:09 +0530

    Update Hero.js

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -7,10 +7,10 @@ function Hero() {
       <div className="hero-content">
         {/* Left Side: Text */}
         <div className="hero-text">
-          <h3>Welcome to my portfolio!</h3>
-          <h2>I am Yogesh K Gowda</h2>
+          <h3>Welcome to my PORTFOLIO!</h3>
+          <h2>Hi I am, Yogesh K Gowda</h2>
           <p>
-            I am a passionate programmer, a data science enthusiast, and a problem solver. 
+            A passionate programmer, a data science enthusiast, and a problem solver. 
             I enjoy writing efficient code, testing software, and building modern web applications. 
             With an analytical mindset, I use data and AI to find useful insights and create solutions that 
             make a difference. My goal is to keep learning, improving, and contributing to the tech world by solving 
